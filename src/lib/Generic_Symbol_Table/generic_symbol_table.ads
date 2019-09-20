2fe98321c9d792be74a5aafd7b973b01033c2c8f
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2019-09-20 12:26:51 +0200

    First draft of symbol table code

---
+++ src/lib/Generic_Symbol_Table/generic_symbol_table.ads
@@ -0,0 +1,14 @@
+
+--
+-- This package provides resources for a generic symbol table that
+-- can prove useful in interpreters.
+--
+-- The model of a s
+--
+
+generic
+   type Symbol_Name is new String;
+   type Symbol_Value (<>) is private;
+package Generic_Symbol_Table is
+
+end Generic_Symbol_Table;
