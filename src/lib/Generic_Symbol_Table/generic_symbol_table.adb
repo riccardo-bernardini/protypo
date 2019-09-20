8322a6de95c5c71aeb09f6d13c668fcf93808117
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2019-09-20 14:14:01 +0200

    First draft body of generic symbol table

---
+++ src/lib/Generic_Symbol_Table/generic_symbol_table.adb
@@ -0,0 +1,127 @@
+pragma Ada_2012;
+
+with Ada.Unchecked_Deallocation;
+
+package body Generic_Symbol_Table is
+
+
+
+
+
+   ----------------
+   -- Open_Block --
+   ----------------
+
+   procedure Open_Block (Table : in out Symbol_Table; Parent : Table_Block) is
+      New_Block : constant Table_Block := new Basic_Block'(Map         => <>,
+                                                           Parent      => Parent,
+                                                           Old_Current => Table.Current);
+   begin
+      Table.Current := New_Block;
+   end Open_Block;
+
+
+   -------------------------
+   -- Open_Internal_Block --
+   -------------------------
+
+   procedure Open_Internal_Block (Table : in out Symbol_Table) is
+   begin
+      Table.Open_Block (Table.Current_Block);
+   end Open_Internal_Block;
+
+   -------------------------
+   -- Open_External_Block --
+   -------------------------
+
+   procedure Open_External_Block (Table : in out Symbol_Table) is
+   begin
+      Table.Open_Block (Table.Root);
+   end Open_External_Block;
+
+
+   -----------------
+   -- Close_Block --
+   -----------------
+
+   procedure Close_Block (Table : in out Symbol_Table)
+   is
+      procedure Free is
+        new Ada.Unchecked_Deallocation (Object => Basic_Block,
+                                        Name   => Table_Block);
+      Old_Current : Table_Block;
+   begin
+      if Table.Current = Table.Root then
+         raise Constraint_Error;
+      end if;
+
+      Old_Current := Table.Current.Old_Current;
+      Free (Table.Current);
+
+      Table.Current := Old_Current;
+   end Close_Block;
+
+   ----------
+   -- Find --
+   ----------
+
+   function Find (Table : Symbol_Table; Name : Symbol_Name) return Cursor is
+      use type Symbol_Maps.Cursor;
+
+      Pos           : Symbol_Maps.Cursor;
+      Current_Trial : Table_Block := Table.Current_Block;
+   begin
+      pragma Assert (Current_Trial /= No_Block);
+
+      while Current_Trial /= No_Block loop
+         Pos := Current_Trial.Map.Find (Name);
+
+         if Pos /= Symbol_Maps.No_Element then
+            return Cursor'(Block           => Current_Trial,
+                           Internal_Cursor => Pos);
+         end if;
+
+         Current_Trial := Current_Trial.Parent;
+      end loop;
+
+      return No_Element;
+   end Find;
+
+   ------------
+   -- Create --
+   ------------
+
+   procedure Create
+     (Table         : in out Symbol_Table;
+      Name          : Symbol_Name;
+      Initial_Value : Symbol_Value)
+   is
+   begin
+      Table.Current.Map.Insert (Key      => Name,
+                                New_Item => Initial_Value);
+   end Create;
+
+   ------------
+   -- Update --
+   ------------
+
+   procedure Update (Pos       : Cursor;
+                     New_Value : Symbol_Value) is
+   begin
+      Pos.Block.Map.Replace_Element (Pos.Internal_Cursor, New_Value);
+   end Update;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   overriding procedure Initialize (Object : in out Symbol_Table) is
+   begin
+      Object.Root := new Basic_Block'(Map         => <>,
+                                      Parent      => No_Block,
+                                      Old_Current => No_Block);
+
+      Object.Current := Object.Root;
+   end Initialize;
+
+end Generic_Symbol_Table;
--- src/lib/Generic_Symbol_Table/generic_symbol_table.ads
+++ src/lib/Generic_Symbol_Table/generic_symbol_table.ads
@@ -1,14 +1,117 @@
+with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Finalization;
 
---
--- This package provides resources for a generic symbol table that
--- can prove useful in interpreters.
---
--- The model of a s
---
+use Ada;
 
 generic
    type Symbol_Name is new String;
    type Symbol_Value (<>) is private;
+   with function Hash (Key : Symbol_Name) return Ada.Containers.Hash_Type;
+   with function Equivalent_Names (X, Y : Symbol_Name) return Boolean;
 package Generic_Symbol_Table is
+   type Symbol_Table is
+     new Finalization.Limited_Controlled
+   with
+     private;
 
+   type Cursor is private;
+   No_Element : constant Cursor;
+
+   type Table_Block is private;
+   No_Block : constant Table_Block;
+
+   function Root (T : Symbol_Table) return Table_Block;
+
+   function Current_Block (T : Symbol_Table) return Table_Block;
+
+   function Parent_Of (T     : Symbol_Table;
+                       Block : Table_Block) return Table_Block
+     with Post => (if Block = T.Root then Parent_Of'Result = No_Block);
+
+
+   procedure Open_Block (Table  : in out Symbol_Table;
+                         Parent : Table_Block)
+     with
+       Pre => Parent /= No_Block,
+       Post => Parent_Of (Table, Table.Current_Block) = Parent;
+
+   procedure Open_internal_Block (Table : in out Symbol_Table);
+   -- Syntactic sugar: Parent defaults to Table.Current_Block
+
+   procedure Open_External_Block (Table : in out Symbol_Table);
+   -- Syntactic sugar: Parent defaults to Table.Root
+
+   procedure Close_Block (Table : in out Symbol_Table)
+     with
+       Pre => Table.Current_Block /= Table.Root;
+
+
+   function Find (Table : Symbol_Table;
+                  Name  : Symbol_Name)
+                  return Cursor;
+
+   function Contains (Table : Symbol_Table;
+                      Name  : Symbol_Name)
+                      return Boolean
+   is (Table.Find (Name) /= No_Element);
+
+   procedure Create (Table         : in out Symbol_Table;
+                     Name          : Symbol_Name;
+                     Initial_Value : Symbol_Value)
+     with
+       Post => Table.Contains (Name);
+
+   procedure Update (Pos       : Cursor;
+                     New_Value : Symbol_Value)
+     with
+       Pre => Pos /= No_Element;
+private
+   package Symbol_Maps is
+     new  Ada.Containers.Indefinite_Hashed_Maps (Key_Type        => Symbol_Name,
+                                                 Element_Type    => Symbol_Value,
+                                                 Hash            => Hash,
+                                                 Equivalent_Keys => Equivalent_Names);
+
+
+
+   type Basic_Block;
+
+   type Table_Block is access Basic_Block;
+   No_Block : constant Table_Block := null;
+
+   type Basic_Block is
+      record
+         Map         : Symbol_Maps.Map;
+         Parent      : Table_Block;
+         Old_Current : Table_Block;
+      end record;
+
+   type Cursor is
+      record
+         Block           : Table_Block;
+         Internal_Cursor : Symbol_Maps.Cursor;
+      end record;
+
+   No_Element : constant Cursor := Cursor'(Block           => No_Block,
+                                           Internal_Cursor => Symbol_Maps.No_Element);
+
+   type Symbol_Table is
+     new Finalization.Limited_Controlled
+   with
+      record
+         Root    : Table_Block;
+         Current : Table_Block;
+      end record;
+
+   overriding procedure Initialize (Object : in out Symbol_Table);
+
+   function Root (T : Symbol_Table) return Table_Block
+   is (T.Root);
+
+   function Current_Block (T : Symbol_Table) return Table_Block
+   is (T.Current);
+
+   function Parent_Of (T     : Symbol_Table;
+                       Block : Table_Block) return Table_Block
+   is (Block.Parent);
 end Generic_Symbol_Table;
