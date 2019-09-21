f9b210562cff5cf98e5b06463460473943573e3d
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2019-09-21 21:59:36 +0200

    minor stuff

--- src/protypo-parsing.adb
+++ src/protypo-parsing.adb
@@ -14,25 +14,32 @@ package body Protypo.Parsing is
    begin
       while not Input.End_Of_Sequence loop
          case Input.Read is
-            when Int =>
-            when Real =>
-            when Text =>
+            when Int | Real | Text | Plus | Minus  Open_Parenthesis =>
+               Parse_Naked_Expression;
+
             when Identifier =>
-            when Plus =>
-            when Minus =>
-            when Open_Parenthesis =>
+               Parse_Name;
+               pragma Compile_Time_Warning (True, "Warning");
+               raise Program_Error;
+
             when Kw_If =>
+               Parse_Conditional;
+
             when Kw_Case =>
-            when Kw_When =>
+               Parse_Case;
+
             when Kw_For =>
             when Kw_Loop =>
             when Kw_Return =>
-            when Mult             |  Div             | Equal |  Different  |
+            when Mult             | Div              | Equal |  Different  |
                  Less_Than        | Greater_Than     | Less_Or_Equal       |
                  Greater_Or_Equal | Assign           | Dot                 |
                  Comma            | End_Of_Statement | Close_Parenthesis   |
-                 Kw_Then           |
-                 Kw_Elsif | Kw_Else | Kw_End | Kw_And | Kw_Or | Kw_In | Kw_Of =>
+                 Kw_Then          | Kw_Elsif         | Kw_Else             |
+                 Kw_End           | Kw_And           | Kw_Or               |
+                 Kw_When          | Kw_In            | Kw_Of =>
+
+               raise Constraint_Error;
          end case;
       end loop;
    end Parse_Statement_Sequence;
