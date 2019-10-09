92271e4e86621093402bc2eb43d3a25fda1e17ee
r-bernardini <framefritti+github@gmail.com>
2019-10-09 23:49:43 +0200

    Added simple_example

---
+++ src/simple_example.adb
@@ -0,0 +1,27 @@
+with Protypo.Api.Interpreters;
+with Protypo.Api.Consumers.File_Writer;
+with Protypo.Api.Engine_Values;
+
+with Callbacks;
+
+procedure Simple_Example is
+   use Protypo.Api;
+   use Protypo.Api.Consumers;
+
+   Engine   : Interpreters.Interpreter_Type;
+
+   Program  : constant Interpreters.Template_Type :=
+                "sin(1.5)=#sin(1.5)#, 42=#the_answer#";
+
+   Consumer : constant Consumer_Access :=
+                File_Writer.Open (File_Writer.Standard_Error);
+
+begin
+   Engine.Define (Name  => "the_answer",
+                  Value => Engine_Values.Create (42));
+
+   Engine.Define (Name  => "sin",
+                  Value => Engine_Values.Create (Callbacks.Sin'Access));
+
+   Engine.Run (Program, Consumer);
+end Simple_Example;
