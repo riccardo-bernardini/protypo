7aba745ca9a1edf2bb1471473709204c706c35b5
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2019-10-22 16:56:28 +0200

    Wrote a bit of documentation

--- src/API/protypo-api-engine_values-array_wrappers.ads
+++ src/API/protypo-api-engine_values-array_wrappers.ads
@@ -1,5 +1,26 @@
 with Protypo.Api.Engine_Values.Basic_Array_Wrappers;
-
+--
+-- ## What is this?
+--
+-- This is a generic package that allows you to export to the template
+-- an "array of something".
+--
+-- The wrapper implements the `Ambivalent_Interface` that allows for both
+-- indexed and record-like access.  More precisely, it exports the following
+-- access methods
+--
+-- * _indexed_ access to access a specific element of the array
+-- * _first_, _last_ and _length_  analoguos to the corresponding Ada
+--   attributes for arrays.
+-- * _range_ and _iterate_ iterators to run over the array content.
+--   _range_ iterates over the set of indexes, _iterate_ over the array
+--   elements
+--
+-- The programmer must provide
+-- * the type of the element
+-- * the type of the array
+-- * a function to convert an element to an `Engine_Value`
+--
 generic
    type Element_Type is private;
 
