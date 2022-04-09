1e494b8b0640f628a381a101b9774cb37aad73e4
Riccardo <framefritti@gmail.com>
2022-04-09 17:35:49 +0200

    Removed debug prints

--- src/API/protypo-api-engine_values-constant_wrappers.adb
+++ src/API/protypo-api-engine_values-constant_wrappers.adb
@@ -1,5 +1,5 @@
 pragma Ada_2012;
-with Ada.Text_Io; use Ada.Text_Io;
+--  with Ada.Text_Io; use Ada.Text_Io;
 
 package body Protypo.Api.Engine_Values.Constant_Wrappers is
 
@@ -16,9 +16,9 @@ package body Protypo.Api.Engine_Values.Constant_Wrappers is
             Result : constant Engine_Value :=
                        Handlers.Create (Handlers.Constant_Interface_Access (Make_Wrapper (Value)));
          begin
-            Put_Line (Result.Class'Image);
-            Put_Line (Boolean'image(Result.Class in Handler_Classes));
-            Put_Line (Boolean'Image (Result in Handler_Value));
+            --  Put_Line (Result.Class'Image);
+            --  Put_Line (Boolean'image(Result.Class in Handler_Classes));
+            --  Put_Line (Boolean'Image (Result in Handler_Value));
             return Result;
          end;
       end if;
